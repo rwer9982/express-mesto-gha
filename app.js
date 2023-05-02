8fa497c879f18d8d8cce6d920f5c8fc796e95852
Evgeniy Makin <rwer9982@yandex.ru>
2023-05-02 08:00:17 +0300

    40v

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use((req, res, next) => {
