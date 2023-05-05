b3aba7b1b2e8fa98d7081b0e114d8f3601794b48
Evgeniy Makin <rwer9982@yandex.ru>
2023-05-05 16:32:34 +0300

    83v

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { joiErrorsCreateUser, joiErrorsLogin } = require('./errors/joiErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const auth = require('./middlewares/auth');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
